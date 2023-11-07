3cad1b47351e83776eb0e09f475134f59e1e8140
daniel-fdez <danief17@ucm.es>
2023-11-07 20:54:25 +0100

    CORRECT

--- 7.1 Navegando sin teclado/navegando.cpp
+++ 7.1 Navegando sin teclado/navegando.cpp
@@ -13,7 +13,7 @@
 #include"DigrafoValorado.h"
 #include"IndexPQ.h"
 using namespace std;
-#define INF 10001
+#define INF 100000000
 
 /*@ <answer>
 
@@ -30,46 +30,31 @@ del tamaño del problema.
 //@ <answer>
 
 
-using Camino = deque<int>;
-
-template<typename Valor>
 class Dijkstra {
 private:
-    // const Valor INF = std::numeric_limits<Valor>::max();
     int origen;
-    vector<Valor>dist;
-    vector<AristaDirigida<Valor>>ulti;
-    IndexPQ<Valor>pq;
+    vector<int>dist;
+    IndexPQ<int>pq;
     vector<int>tiempos;
-    void relajar(AristaDirigida<Valor>a) {
+    void relajar(AristaDirigida<int>a) {
     int v = a.desde(), w = a.hasta();
-    if (dist[w] > dist[v] + a.valor()) {
+    if (dist[w] > dist[v] + a.valor() + tiempos[a.hasta()]) {
         dist[w] = dist[v] + a.valor() + tiempos[a.hasta()];
-        ulti[w] = a;
         pq.update(w, dist[w]);
     }
 }
 public:
-    Dijkstra(DigrafoValorado<Valor> const&g, int orig, vector<int>const& t) : origen(orig), dist(g.V(), INF), ulti(g.V()), pq(g.V()), tiempos(t) {
-        dist[origen] = tiempos[origen];
-        pq.push(origen, 0);
+    Dijkstra(DigrafoValorado<int> const&g, vector<int>const& t) : origen(0), dist(g.V(), INF), pq(g.V()), tiempos(t) {
+        dist[origen] = tiempos[0];
+        pq.push(origen, tiempos[0]);
         while (!pq.empty()) {
             int v = pq.top().elem; pq.pop();
-            for (auto a : g.ady(v))
+            for (auto a : g.ady(v)) 
                 relajar(a);
         }
     }
     bool hayCamino(int v) const { return dist[v] != INF; }
-    Valor distancia(int v) const { return dist[v]; }
-
-    // Camino<Valor> camino(int v) const {
-    //     Camino<valor> cam;
-    //     AristaDirigida<Valor>a;
-    //     for (a = ulti[v]; a.desde() != origen; a = ulti[a.desde()])
-    //         cam.push_front(a);
-    //     cam.push_front(a);
-    //     return cam;
-    // }
+    int distancia(int v) const { return dist[v]; }
 };
 
 
@@ -91,7 +76,7 @@ bool resuelveCaso() {
         grafo.ponArista({a - 1, b - 1, c});
     }
 
-    Dijkstra<int>d(grafo, 0, tiempos_de_carga);
+    Dijkstra d(grafo, tiempos_de_carga);
 
     if (d.hayCamino(n - 1)) cout << d.distancia(n - 1) << "\n";
     else cout << "IMPOSIBLE\n";
