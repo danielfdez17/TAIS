ffccbc2300941e911f931018c4ae0521d42e3e0d
danielfdez17 <daniel17dev@gmail.com>
2023-12-29 10:01:07 +0100

    template updated

--- Templates/Sem 3 Heapsort y colas de prioridad con prioridades variables/IndexPQ.h
+++ Templates/Sem 3 Heapsort y colas de prioridad con prioridades variables/IndexPQ.h
@@ -18,42 +18,37 @@
 #include <iostream>
 #include <stdexcept>
 #include <vector>
+#include <unordered_map>
 
 
 // T es el tipo de las prioridades
-// Comparator dice cuÃ¡ndo un valor de tipo T es mÃ¡s prioritario que otro
-template <typename T = int, typename Comparator = std::less<T>>
+// Comparator dice cuándo un valor de tipo T es más prioritario que otro
+template <typename ID, typename T, typename Comparator = std::less<T>>
 class IndexPQ {
 public:
    // registro para las parejas < elem, prioridad >
    struct Par {
-      int elem;
+      ID elem;
       T prioridad;
    };
    
 private:
    // vector que contiene los datos (pares < elem, prio >)
-   std::vector<Par> array;     // primer elemento en la posiciÃ³n 1
+   std::vector<Par> array;     // primer elemento en la posición 1
    
-   // vector que contiene las posiciones en array de los elementos
-   std::vector<int> posiciones;   // un 0 indica que el elemento no estÃ¡
+   // diccionario que contiene las posiciones en array de los elementos
+   std::unordered_map<ID, int> posiciones;
    
-   /* Objeto funciÃ³n que sabe comparar prioridades.
-    antes(a,b) es cierto si a es mÃ¡s prioritario que b */
+   /* Objeto función que sabe comparar prioridades.
+    antes(a,b) es cierto si a es más prioritario que b */
    Comparator antes;
    
 public:
-   IndexPQ(int N, Comparator c = Comparator()) : array(1), posiciones(N, 0), antes(c) {}
+   IndexPQ(Comparator c = Comparator()) :
+       array(1), antes(c) {}
    
-   IndexPQ(IndexPQ<T,Comparator> const&) = default;
-   
-   IndexPQ<T,Comparator>& operator=(IndexPQ<T,Comparator> const&) = default;
-   
-   ~IndexPQ() = default;
-   
-   // e debe ser uno de los posibles elementos
-   void push(int e, T const& p) {
-      if (posiciones.at(e) != 0)
+   void push(ID e, T const& p) {
+      if (posiciones.count(e))
          throw std::invalid_argument("No se pueden insertar elementos repetidos.");
       else {
          array.push_back({e, p});
@@ -62,11 +57,11 @@ public:
       }
    }
    
-   void update(int e, T const& p) {
-      int i = posiciones.at(e);
-      if (i == 0) // el elemento e se inserta por primera vez
+   void update(ID e, T const& p) {
+      if (!posiciones.count(e)) // el elemento e se inserta por primera vez
          push(e, p);
       else {
+         int i = posiciones[e];
          array[i].prioridad = p;
          if (i != 1 && antes(array[i].prioridad, array[i/2].prioridad))
             flotar(i);
@@ -90,9 +85,9 @@ public:
    }
    
    void pop() {
-      if (size() == 0) throw std::domain_error("No se puede eliminar el primero de una cola vacÃ­a.");
+      if (size() == 0) throw std::domain_error("No se puede eliminar el primero de una cola vacía.");
       else {
-         posiciones[array[1].elem] = 0; // para indicar que no estÃ¡
+         posiciones.erase(array[1].elem); // eliminamos el elemento
          if (size() > 1) {
             array[1] = std::move(array.back());
             posiciones[array[1].elem] = 1;
@@ -123,7 +118,7 @@ private:
          // cambiar al hijo derecho de i si existe y va antes que el izquierdo
          if (hijo < size() && antes(array[hijo + 1].prioridad, array[hijo].prioridad))
             ++hijo;
-         // flotar el hijo si va antes que el elemento hundiÃ©ndose
+         // flotar el hijo si va antes que el elemento hundiéndose
          if (antes(array[hijo].prioridad, parmov.prioridad)) {
             array[hueco] = std::move(array[hijo]); posiciones[array[hueco].elem] = hueco;
             hueco = hijo; hijo = 2*hueco;
